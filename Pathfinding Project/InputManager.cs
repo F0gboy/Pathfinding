048ada0f3bcb3575982451e803dbe45875cdb2e5
F0gboy <Jasp3r@live.dk>
2024-02-22 13:13:59 +0100

    fe

--- Pathfinding Project/InputManager.cs
+++ Pathfinding Project/InputManager.cs
@@ -14,8 +14,8 @@ namespace Pathfinding_Project
         {
             var mouseState = Mouse.GetState();
 
-            MouseClicked = mouseState.LeftButton == ButtonState.Pressed && _lastMouseState.LeftButton == ButtonState.Released;
-            MouseRightClicked = mouseState.RightButton == ButtonState.Pressed && _lastMouseState.RightButton == ButtonState.Released;
+            //MouseClicked = mouseState.LeftButton == ButtonState.Pressed && _lastMouseState.LeftButton == ButtonState.Released;
+            //MouseRightClicked = mouseState.RightButton == ButtonState.Pressed && _lastMouseState.RightButton == ButtonState.Released;
             MouseRectangle = new(mouseState.Position.X, mouseState.Position.Y, 1, 1);
 
             _lastMouseState = mouseState;
